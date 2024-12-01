72523cec2f1f0f045688cd8b940d1921f77f7587
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-11-30 22:06:45 -0600

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -9,29 +9,25 @@ let hostList = [];
 
 export async function serverRequestResponse(reqDTO){
   
-  let resDTO={};
-  resDTO.headers={};
-  let hostProxy = reqDTO.host;
-  hostTarget=hostProxy.replace('-dynamic-router-servleteer.vercel.app','').replaceAll('-','.');
-  if(globalThis[hostTarget]){
-    await globalThis[hostTarget];
-  }else{
-    globalThis[hostTarget]=import('https://files-servleteer.vercel.app/'+hostTarget+'/'+hostTarget+'.js');
-    await globalThis[hostTarget];
-  }
+  const resDTO = {
+    headers : {}
+  };
+  const hostProxy = reqDTO.host;
+  hostTarget = hostProxy.replace('-dynamic-router-servleteer.vercel.app','').replaceAll('-','.');
+  globalThis[hostTarget] ??= await import('https://files-servleteer.vercel.app/'+hostTarget+'/'+hostTarget+'.js');
   hostList.push(hostTarget);
-  let path = reqDTO.shortURL.replaceAll('*', '');
+  const path = reqDTO.shortURL.replaceAll('*', '');
   let pat = path.split('?')[0].split('#')[0];
 
-  if (reqDTO.shortURL == '/ping') {
+  if (reqDTO.shortURL === '/ping') {
     resDTO.statusCode = 200;
     return resDTO;
   }
-  if (pat == '/link-resolver.js') {
-    let response = await fileFetch(pat);
+  
+  if (pat === '/link-resolver.js') {
+    const response = await fileFetch(pat);
     resDTO = mapResDTO(resDTO,response);
     return resDTO;
-
   }
 
   reqDTO.host = hostTarget;
@@ -39,49 +35,35 @@ export async function serverRequestResponse(reqDTO){
   reqDTO.headers.referer = hostTarget;
 
     /* fetch from your desired target */
-    let response = await fetch('https://' + hostTarget + path,reqDTO);
+    const response = await fetch('https://' + hostTarget + path,reqDTO);
 
     /* if there is a problem try redirecting to the original */
     if (response.status > 399) {
-      resDTO.headers['location']='https://' + hostTarget + path;
+      resDTO.headers['location'] = 'https://' + hostTarget + path;
       resDTO.statusCode = 302;
       return resDTO;
     }
-
-
+    
     resDTO = mapResDTO(resDTO,response);
 
-    
     /* check to see if the response is not a text format */
-    let ct = response.headers.get('content-type');
-
-  
+    const ct = response.headers.get('content-type');
     resDTO.headers['content-type'] = ct;
-  
-
     
-    if ((ct) && (!ct.includes('image')) && (!ct.includes('video')) && (!ct.includes('audio'))) {
-
+    if (!/image|video|audio/i.test(ct)){
       /* Copy over target response and return */
       let resBody = await response.text();
-if(ct.includes('html')||ct.includes('xml')||pat.endsWith('.html')||pat.endsWith('.xhtml')){
+    if(/html|xml/i.test(ct)){
       resBody = resBody.replace('<head>',
         `<head modified><script src=https://`+ hostProxy + `/link-resolver.js host-list=` + btoa(JSON.stringify(hostList)) + `></script>`);
     }
       resDTO['Content-Length']=new Blob([resBody]).size;
       resDTO.body = resBody;
       return resDTO;
-
-
     } else {
-      
-      let resBody = Buffer.from(await response.arrayBuffer());
-      resDTO['Content-Length']=resBody.length;
+      const resBody = Buffer.from(await response.arrayBuffer());
+      resDTO['Content-Length'] = resBody.length;
       resDTO.body = resBody;
       return resDTO;
-
     }
-  
-
-
 }
